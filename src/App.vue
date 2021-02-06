c5c8f715915f6c56ca3dfd426c91d0bf8ea88001
lexyhandi <lexihandi@gmail.com>
2021-02-06 16:44:38 +0700

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <Title title="Catatan Pengeluaran" />
+  <ListPengeluaran
+    v-if="dataPengeluaran.length > 0"
+    :dataPengeluaran="dataPengeluaran"
+  />
+</template>
+
+<script>
+import Title from "./components/Title";
+import ListPengeluaran from "./components/ListPengeluaran";
+export default {
+  name: "App",
+  components: {
+    Title,
+    ListPengeluaran,
+  },
+  data() {
+    return {
+      dataPengeluaran: [
+        { nominal: 20000, keterangan: "Makan Siang" },
+        { nominal: 20000, keterangan: "Makan Malam" },
+        { nominal: 15000, keterangan: "Makan Pagi" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ListPengeluaran.vue
@@ -0,0 +1,44 @@
+<template>
+  <table>
+    <tr>
+      <th>Pengeluaran</th>
+      <th>Keterangan</th>
+    </tr>
+    <tr v-for="(pengeluaran, index) in dataPengeluaran" :key="index">
+      <td>{{ pengeluaran.nominal }}</td>
+      <td>{{ pengeluaran.keterangan }}</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    dataPengeluaran: Array,
+  },
+};
+</script>
+
+<style scoped>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+table,
+th,
+td {
+  border: 1px solid black;
+}
+th {
+  background-color: magenta;
+  color: white;
+}
+td {
+  background-color: orange;
+  color: black;
+}
+tr:hover td {
+  background-color: white;
+  cursor: pointer;
+}
+</style>
